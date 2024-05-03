c387c73d4f004fd2414858ad9a8cd997f9966462
Ruseik Rahumath <119164618+Ruseik@users.noreply.github.com>
2024-05-03 06:24:44 +0530

    Initial - push - 2024/05/03

---
+++ application/models/HDPrescription_model.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//This Script is responsible to talk with database to get and update the data relavent to HD Prescriptions
+class HDPrescription_model extends CI_Model {
+
+    //Load the basics
+}//Script is responsible to talk with database to get and update the data relavent to HD Prescriptions end here!
